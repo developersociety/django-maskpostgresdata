b455b6051cdf4f60a5fb86f9b546c7cc2c75bd06
alistairclark <alistairclark89@gmail.com>
2019-08-02 14:00:35 +0100

    Add a readme and some other stuff

---
+++ README.rst
@@ -0,0 +1,17 @@
+=========================
+Django Mask Postgres Data
+=========================
+
+Adds a management command to your Django project which allows you to create a (sort of) pg_dump
+of your data, with sepcific fields masked by given values.
+
+To configure, add a dictionary called `MASKER_FIELDS` to your settings using the following format:
+
+```
+
+MASKER_FIELDS = {
+    "{ APP_NAME }": {"{ MODEL_NAME }": {"{ FIELD_NAME }": { VALUE},}},
+}
+```
+
+You can then run `manage.py dump_masked_data` and it will dump your data to `stdout`.
--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
 from setuptools import setup
 
-setup(name='django-maskpostgresdata',
-      version='0.1',
-      description='Creates a pg_dumpish output which masks data without saving changes to the source database.',
-      url='https://github.com/developersociety/django-maskpostgresdata',
-      author='Alistair Clrk',
-      author_email='alistair@dev.ngo',
-      license='MIT',
-      packages=['funniest'],
-      zip_safe=False)
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(
+    name='django-maskpostgresdata',
+    version='0.1',
+    description='Creates a pg_dumpish output which masks data without saving changes to the source database.',
+    url='https://github.com/developersociety/django-maskpostgresdata',
+    author='Developer Society',
+    author_email='hello@dev.ngo',
+    license='BSD',
+    zip_safe=False,
+    requires = [
+        'django>=1.8',
+        'psycopg2'
+    ]
+)
