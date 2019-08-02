20443e1c0aa549dd74cf53eae6c2f32b10db0487
alistairclark <alistairclark89@gmail.com>
2019-08-02 15:07:14 +0100

    Add long description from README

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup
 
 def readme():
-    with open('README.rst') as f:
-        return f.read()
+    with open('README.md') as f:
+        long_description = f.read()
 
 setup(
     name='django-maskpostgresdata',
     packages=['maskpostgresdata.management'],
     version='0.1.3',
     description='Creates a pg_dumpish output which masks data without saving changes to the source database.',
+    long_description=long_description,
     url='https://github.com/developersociety/django-maskpostgresdata',
     author='Developer Society',
     author_email='hello@dev.ngo',
