97e6d652e3136295f8843f98f9e48ce5512bb907
alistairclark <alistairclark89@gmail.com>
2019-08-02 15:10:13 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open('README.md') as f:
 setup(
     name='django-maskpostgresdata',
     packages=['maskpostgresdata.management'],
-    version='0.1.3',
+    version='0.1.4',
     description='Creates a pg_dumpish output which masks data without saving changes to the source database.',
     long_description=long_description,
     long_description_content_type="text/markdown",
