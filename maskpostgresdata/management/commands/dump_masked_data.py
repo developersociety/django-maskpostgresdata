b58c5db66e055e2a8465c167c77e69415823a837
alistairclark <alistairclark89@gmail.com>
2019-08-14 14:28:27 +0100

    use _default_manager instead of queryset

--- maskpostgresdata/management/commands/dump_masked_data.py
+++ maskpostgresdata/management/commands/dump_masked_data.py
@@ -71,8 +71,8 @@ class Command(BaseCommand):
         for app in fields_to_mask.keys():
             for model, fields in fields_to_mask[app].items():
                 model_class = apps.get_model(app.lower(), model_name=model.lower())
-                model_class.objects.update(**fields)
-                table_name = model_class.objects.model._meta.db_table
+                model_class._default_manager.update(**fields)
+                table_name = model_class._default_manager.model._meta.db_table
 
                 altered_tables.append(table_name)
                 print("COPY public.{} FROM stdin;".format(table_name), file=self.stdout._out)                
@@ -81,7 +81,7 @@ class Command(BaseCommand):
 
         for app in apps.get_app_configs():
             for model in app.get_models():
-                table_name = model.objects.model._meta.db_table
+                table_name = model._default_manager.model._meta.db_table
                 if table_name not in altered_tables:
                     print("COPY public.{} FROM stdin;".format(table_name), file=self.stdout._out)                
                     cursor.copy_to(self.stdout._out, table_name)
