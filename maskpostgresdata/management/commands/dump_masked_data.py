606d9eb9bc888b13d631f7ea77f6da9d58c28e16
Edvinas Jurevicius <ikonitas@gmail.com>
2020-02-20 15:13:37 +0000

    Fix sentry-issue#1336457724

--- maskpostgresdata/management/commands/dump_masked_data.py
+++ maskpostgresdata/management/commands/dump_masked_data.py
@@ -1,3 +1,4 @@
+import sys
 import os
 import subprocess
 
@@ -49,86 +50,91 @@ class Command(BaseCommand):
     def handle(self, **options):
         connection = connections[options["database"]]
 
-        args = ["pg_dump"]
+        try:
+            args = ["pg_dump"]
 
-        conn_params = connection.get_connection_params()
-        host = conn_params.get("host", "")
-        port = conn_params.get("port", "")
-        dbname = conn_params.get("database", "")
-        user = conn_params.get("user", "")
-        passwd = conn_params.get("password", "")
+            conn_params = connection.get_connection_params()
+            host = conn_params.get("host", "")
+            port = conn_params.get("port", "")
+            dbname = conn_params.get("database", "")
+            user = conn_params.get("user", "")
+            passwd = conn_params.get("password", "")
 
-        if user:
-            args += ["-U", user]
-        if host:
-            args += ["-h", host]
-        if port:
-            args += ["-p", str(port)]
-        args += [dbname]
+            if user:
+                args += ["-U", user]
+            if host:
+                args += ["-h", host]
+            if port:
+                args += ["-p", str(port)]
+            args += [dbname]
 
-        subprocess_env = os.environ.copy()
-        if passwd:
-            subprocess_env["PGPASSWORD"] = str(passwd)
+            subprocess_env = os.environ.copy()
+            if passwd:
+                subprocess_env["PGPASSWORD"] = str(passwd)
 
-        masker_args = getattr(settings, "MASKER_ARGS", ["--no-owner", "--no-privileges"])
-        if masker_args:
-            args += masker_args
+            masker_args = getattr(settings, "MASKER_ARGS", ["--no-owner", "--no-privileges"])
+            if masker_args:
+                args += masker_args
 
-        connection.ensure_connection()
-        connection.set_autocommit(False)
+            connection.ensure_connection()
+            connection.set_autocommit(False)
 
-        if connection.isolation_level != ISOLATION_LEVEL_SERIALIZABLE:
-            connection.connection.set_session(isolation_level=ISOLATION_LEVEL_SERIALIZABLE)
-            connection.isolation_level = ISOLATION_LEVEL_SERIALIZABLE
-            connection.connection.isolation_level = ISOLATION_LEVEL_SERIALIZABLE
+            if connection.isolation_level != ISOLATION_LEVEL_SERIALIZABLE:
+                connection.connection.set_session(isolation_level=ISOLATION_LEVEL_SERIALIZABLE)
+                connection.isolation_level = ISOLATION_LEVEL_SERIALIZABLE
+                connection.connection.isolation_level = ISOLATION_LEVEL_SERIALIZABLE
 
-        cursor = connection.cursor()
-        cursor.execute("SELECT pg_export_snapshot();")
-        snapshot_id = cursor.fetchone()[0]
+            cursor = connection.cursor()
+            cursor.execute("SELECT pg_export_snapshot();")
+            snapshot_id = cursor.fetchone()[0]
 
-        args += ["--snapshot={}".format(snapshot_id)]
+            args += ["--snapshot={}".format(snapshot_id)]
 
-        header_dump = args + ["--section=pre-data"]
-        subprocess.run(header_dump, stdout=self.stdout._out, env=subprocess_env)
+            header_dump = args + ["--section=pre-data"]
+            subprocess.run(header_dump, stdout=self.stdout._out, env=subprocess_env)
 
-        fields_to_mask = getattr(settings, "MASKER_FIELDS", None)
-        altered_tables = []
+            fields_to_mask = getattr(settings, "MASKER_FIELDS", None)
+            altered_tables = []
 
-        for app in apps.get_app_configs():
-            for model in app.get_models():
-                table_name = model._default_manager.model._meta.db_table
-                if hasattr(self, 'update_{}'.format(table_name)):
-                    getattr(self, 'update_{}'.format(table_name))(model._default_manager.all())
+            for app in apps.get_app_configs():
+                for model in app.get_models():
+                    table_name = model._default_manager.model._meta.db_table
+                    if hasattr(self, 'update_{}'.format(table_name)):
+                        getattr(self, 'update_{}'.format(table_name))(model._default_manager.all())
 
-        if fields_to_mask:
-            for app in fields_to_mask.keys():
-                for model, fields in fields_to_mask[app].items():
-                    model_class = apps.get_model(app.lower(), model_name=model.lower())
-                    model_class._default_manager.update(**fields)
-                    table_name = model_class._default_manager.model._meta.db_table
+            if fields_to_mask:
+                for app in fields_to_mask.keys():
+                    for model, fields in fields_to_mask[app].items():
+                        model_class = apps.get_model(app.lower(), model_name=model.lower())
+                        model_class._default_manager.update(**fields)
+                        table_name = model_class._default_manager.model._meta.db_table
 
-                    altered_tables.append(table_name)
-                    print("COPY public.{} FROM stdin;".format(table_name), flush=True)
-                    cursor.copy_to(self.stdout._out, table_name)
-                    print("\\.\n", file=self.stdout._out, flush=True)
+                        altered_tables.append(table_name)
+                        print("COPY public.{} FROM stdin;".format(table_name), flush=True)
+                        cursor.copy_to(self.stdout._out, table_name)
+                        print("\\.\n", file=self.stdout._out, flush=True)
 
-        for app in apps.get_app_configs():
-            for model in app.get_models():
-                table_name = model._default_manager.model._meta.db_table
+            for app in apps.get_app_configs():
+                for model in app.get_models():
+                    table_name = model._default_manager.model._meta.db_table
 
-                if table_name not in altered_tables:
-                    print("COPY public.{} FROM stdin;".format(table_name), flush=True)
-                    cursor.copy_to(self.stdout._out, table_name)
-                    print("\\.\n", file=self.stdout._out, flush=True)
+                    if table_name not in altered_tables:
+                        print("COPY public.{} FROM stdin;".format(table_name), flush=True)
+                        cursor.copy_to(self.stdout._out, table_name)
+                        print("\\.\n", file=self.stdout._out, flush=True)
 
-        print("COPY public.django_migrations FROM stdin;".format(table_name), flush=True)
-        cursor.copy_to(self.stdout._out, 'django_migrations')
-        print("\\.\n", file=self.stdout._out, flush=True)
+            print("COPY public.django_migrations FROM stdin;".format(table_name), flush=True)
+            cursor.copy_to(self.stdout._out, 'django_migrations')
+            print("\\.\n", file=self.stdout._out, flush=True)
 
-        # Sets a new values for sequences.
-        self.reset_sequences(cursor)
+            # Sets a new values for sequences.
+            self.reset_sequences(cursor)
 
-        post_data_dump = args + ["--section=post-data"]
-        subprocess.run(post_data_dump, stdout=self.stdout._out, env=subprocess_env)
+            post_data_dump = args + ["--section=post-data"]
+            subprocess.run(post_data_dump, stdout=self.stdout._out, env=subprocess_env)
 
-        transaction.rollback()
+            transaction.rollback()
+        except (KeyboardInterrupt, BrokenPipeError):
+            print("\n", file=self.stdout._out, flush=True)
+            print("Keyboard interaction detected", file=self.stdout._out, flush=True)
+            sys.exit(0)
